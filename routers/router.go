3934b2fb0cd9e005fe2822d675b3dfa63f8f5126
Andrew Qi <qf353208262@gmail.com>
2020-10-13 15:02:10 +0800

    feat: add front-end

--- routers/router.go
+++ routers/router.go
@@ -14,6 +14,7 @@ import (
 )
 
 func init() {
+	beego.Router("/",&controllers.IndexController{})
 	beego.Router("api/goods/list", &controllers.ProductListController{})
 	beego.Router("api/goods/:id", &controllers.ProductDetailController{})
 
